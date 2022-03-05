680369a2f7659957fd29fdadd947ef8513afc37a
UliaKarpova <chelovek-e-95@mail.ru>
2022-03-05 13:45:29 +0300

    fixes after review

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__edit-button');
 openPopup.addEventListener('click', function() {
-    popup.classList.add('popup__opened')
+    popup.classList.add('popup_opened')
 });
 
 let formElement = document.querySelector('.popup__edit-button');
@@ -18,13 +18,13 @@ function formSubmitHandler(evt) {
     let info = infoInput.value;
     profileName.textContent = name;
     profileInfo.textContent = info;
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 formElement.addEventListener('submit', formSubmitHandler);
 
 const closePopup = document.querySelector('.popup__exit');
 closePopup.addEventListener('click', function() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
     nameInput.value = profileName.textContent;
     infoInput.value = profileInfo.textContent;
 });
