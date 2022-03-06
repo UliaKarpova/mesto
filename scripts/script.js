5b6da00ed0205d11b3885b7a97c350a69c9f5eba
UliaKarpova <chelovek-e-95@mail.ru>
2022-03-06 16:18:39 +0300

    fix popup&profile

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.profile__edit-button');
-openPopup.addEventListener('click', function() {
-    popup.classList.add('popup_opened')
-});
+const editButton = document.querySelector('.profile__edit-button');
 
 let formElement = document.querySelector('.popup__edit-button');
 let nameInput = document.querySelector('#name');
@@ -12,12 +9,16 @@ let profileInfo = document.querySelector('.profile__text');
 nameInput.value = profileName.textContent;
 infoInput.value = profileInfo.textContent;
 
+editButton.addEventListener('click', function() {
+    nameInput.value = profileName.textContent;
+    infoInput.value = profileInfo.textContent;
+    popup.classList.add('popup_opened')
+});
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let name = nameInput.value;
-    let info = infoInput.value;
-    profileName.textContent = name;
-    profileInfo.textContent = info;
+    profileName.textContent = nameInput.value;
+    profileInfo.textContent = infoInput.value;
     popup.classList.remove('popup_opened');
 }
 formElement.addEventListener('submit', formSubmitHandler);
@@ -25,7 +26,5 @@ formElement.addEventListener('submit', formSubmitHandler);
 const closePopup = document.querySelector('.popup__exit');
 closePopup.addEventListener('click', function() {
     popup.classList.remove('popup_opened');
-    nameInput.value = profileName.textContent;
-    infoInput.value = profileInfo.textContent;
 });
 
